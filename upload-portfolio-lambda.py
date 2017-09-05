07a25257c51f8753e1d5fbe02c03bdb2ab250628
Mike Onan <mike@onanlabs.com>
2017-09-05 18:27:35 -0500

    updated lambda function for error handling

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,20 +4,29 @@ import StringIO
 import zipfile
 
 def lambda_handler(event, context):
-    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:012822635068:deployPortfolioTopic')
+
+    try:
+        s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+        portfolio_bucket = s3.Bucket('onanlabs.com')
+        build_bucket = s3.Bucket('portfoliobuild.onanlabs.com')
 
 
-    portfolio_bucket = s3.Bucket('onanlabs.com')
-    build_bucket = s3.Bucket('portfoliobuild.onanlabs.com')
+        portfolio_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
+        with zipfile.ZipFile(portfolio_zip) as myzip:
+          for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm)
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
-    portfolio_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+        print "Job Complete!"
+        topic.publish(Subject="Portfolio Deployed", Message="Portfolio deployed sucessfully!")
+    except:
+        topic.publish(Subject="Portfolio Deploy Failed", Message="The Portfolio was not deployed successfully!")
+        raise
 
-    with zipfile.ZipFile(portfolio_zip) as myzip:
-      for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm)
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-    print "Job Complete!"
     return 'Hello from Lambda'
