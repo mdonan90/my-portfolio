c113db784d433741bf7a183de83f1c8fa2305414
Mike Onan <mike@onanlabs.com>
2017-09-05 18:05:33 -0500

    updated code again

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -2,8 +2,6 @@ import boto3
 from botocore.client import Config
 import StringIO
 import zipfile
-import mimetypes
-
 
 def lambda_handler(event, context):
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
@@ -19,8 +17,7 @@ def lambda_handler(event, context):
     with zipfile.ZipFile(portfolio_zip) as myzip:
       for nm in myzip.namelist():
         obj = myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.upload_fileobj(obj, nm)
         portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
     print "Job Complete!"
     return 'Hello from Lambda'
