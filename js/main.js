af2de54e36beda628175c36ce697766b5ad1dc34
Mike Onan <mike@onanlabs.com>
2017-09-18 14:50:46 -0500

    Move static markup for work examples into React Components and cleaned up other files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+  {
+    'title': "Connect Calculator",
+    'image': {
+      'desc': "Connect Calculator",
+      'src': "images/connectcalc.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "Video Transcoder",
+    'image': {
+      'desc': "Video Transcoder",
+      'src': "images/video.png",
+      'comment': ""
+    }
+  },
+  {
+    'title': "URL Shortener",
+    'image': {
+      'desc': "URL Shortener",
+      'src': "images/url.png",
+      'comment': ""
+    }
+  }
+]
+
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
